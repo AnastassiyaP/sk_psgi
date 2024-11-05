d61331f44c7b9e729a1873c9f5698ab2d1bb8b46
Anastasiia Medvedeva <anastasiia.medvedeva2@gmail.com>
2024-11-05 15:11:18 +0500

    fix

--- sql/opt.sql
+++ sql/opt.sql
@@ -14,14 +14,14 @@ CREATE TABLE `actions_v2` (
 
 
 CREATE TABLE `coupon` (
-  `id` int unsigned NOT NULL  PRIMARY KEY COMMENT 'Номер купона',
+  `id` int unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'ID купона',
   `code` varchar(256) NOT NULL DEFAULT '0' COMMENT 'Номер карты, купона или промокод',
   `action_id` int unsigned NOT NULL DEFAULT '0' COMMENT 'Номер акции',
   `start_date` datetime DEFAULT NULL COMMENT 'Время старта акции',
   `end_date`   datetime DEFAULT NULL COMMENT 'Время окончания акции',
-  `type` enum('card', 'coupon', 'promocode') COMMENT 'Ттип значения в поле code',
+  `type` enum('card', 'coupon', 'promocode') NOT NULL COMMENT 'Ттип значения в поле code',
   `placeholders` text DEFAULT NULL  COMMENT 'JSON c плейсхолдерами для акций в формате {"NAME": "Александра"} или пустая строка',
-  unique KEY (`code`, `action_id`,`type`),
+  unique KEY (`code`, `action_id`),
   KEY (`action_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
