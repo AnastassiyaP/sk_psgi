04cf264c2e8378fa53bead517c4d896cf6552e22
Anastasiia Medvedeva <anastasiia.medvedeva2@gmail.com>
2025-01-04 15:47:55 +0500

    sql fixes

--- sql/coupon.sql
+++ sql/coupon.sql
@@ -1,11 +1,11 @@
 CREATE TABLE `coupon` (
-  `id` INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'ID купона',
-  `code` varchar(256) NOT NULL DEFAULT '0' COMMENT 'Номер карты, купона или промокод',
+  `id` BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'ID купона',
+  `code` varchar(20) NOT NULL DEFAULT '0' COMMENT 'Номер карты, купона или промокод',
   `action_id` int unsigned NOT NULL DEFAULT '0' COMMENT 'Номер акции',
   `start_date` datetime DEFAULT NULL COMMENT 'Время старта акции',
   `end_date`   datetime DEFAULT NULL COMMENT 'Время окончания акции',
-  `placeholders` json DEFAULT NULL  COMMENT 'JSON c плейсхолдерами для акций в формате {"NAME": "Александра"} или пустая строка',
-  unique KEY (`code`, `action_id`),
+  `placeholders` json DEFAULT NULL  COMMENT 'JSON c плейсхолдерами для акций в формате {"NAME": "Александра"} или NULL',
+  unique KEY (`action_id`,`code`),
   KEY (`action_id`)
 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
--- sql/coupon_usage.sql
+++ sql/coupon_usage.sql
@@ -1,14 +1,14 @@
 CREATE TABLE `coupon_usage` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID  погашения купона',
-  `coupon_id` int unsigned NOT NULL DEFAULT '0' COMMENT 'ID купона или связи карты и акции',
+  `id` BIGINT unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID  погашения купона',
+  `coupon_id` BIGINT unsigned NOT NULL DEFAULT '0' COMMENT 'ID купона или связи карты и акции',
   `card_number` bigint unsigned NOT NULL DEFAULT '0' COMMENT 'Номер карты',
-  `uniq_key` varchar(256) NOT NULL COMMENT 'уникальный id запроса - номер корзины или чека',
+  `uniq_key` varchar(20) NOT NULL COMMENT 'уникальный id запроса - номер корзины или чека',
   `shop_id` int unsigned NOT NULL DEFAULT '0' COMMENT 'id магазина',
   `receipt_ts` timestamp NULL DEFAULT NULL COMMENT 'время события на кассе',
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Время записи',
   `status` enum('new','holdout','canceled','accepted') DEFAULT 'new',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `key_card_number` (`uniq_key`,`card_number`,`coupon_id`),
+  UNIQUE KEY `key_card_number` (`uniq_key`,`coupon_id`),
   KEY `card_number` (`card_number`),
   KEY `shop_id` (`shop_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2061 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
